1943b2ce8405ec163b302969c6ad1067bcc547ca
Muhammad Yaqoob <muhammadyaqoob2580@gmail.com>
2025-11-20 13:18:59 +0500

    fixed issues in routes

--- dist/routes/dashboardRoutes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const authMiddleware_1 = require("../middlewares/authMiddleware");
-const dashboardController_1 = require("../controllers/dashboardController");
-const router = (0, express_1.Router)();
-// All dashboard routes require login
-router.get("/stats", authMiddleware_1.auth, dashboardController_1.getDashboardStats);
-exports.default = router;
